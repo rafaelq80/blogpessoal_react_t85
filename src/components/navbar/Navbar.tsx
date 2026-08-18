1b08bf2560ccb534a806cb1f51d6d1ae8f313169
Rafael Queiroz <rafaelproinfo@gmail.com>
2026-08-18 09:00:13 -0300

    💄 UI:  Criação de Rotas

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 
 function Navbar() {
 
@@ -7,14 +8,14 @@ function Navbar() {
             			   bg-indigo-900 text-white"
 			>
 				<div className="container flex justify-between text-lg mx-8">
-					Blog Pessoal
+					<Link to="/" className="text-2xl font-bold">Blog Pessoal</Link>
 
 					<div className="flex gap-4">
             Postagens
             Temas
             Cadastrar tema
             Perfil
-            Sair
+            <Link to="/login" className="hover:underline">Sair</Link>
           </div>
 				</div>
 			</div>
